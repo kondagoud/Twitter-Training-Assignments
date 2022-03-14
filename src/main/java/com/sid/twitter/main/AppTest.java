9ef1ef4a9e1eab4855ca10fc15da691f05764269
kondagoud <kondagoud.merugu@atmecs.com>
2022-03-14 12:04:07 -0700

    updated sum method parameter

--- src/main/java/com/sid/twitter/main/AppTest.java
+++ src/main/java/com/sid/twitter/main/AppTest.java
@@ -2,7 +2,7 @@ package com.sid.twitter.main;
 
 public class AppTest {
     public static void main(String[] args) {
-        sum(10, 30);
+        sum(11, 40);
     }
     public static void sum(int a, int b){
         System.out.println(a+b);
