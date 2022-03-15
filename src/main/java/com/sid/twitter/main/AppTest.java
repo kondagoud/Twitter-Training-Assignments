12357f7b60135879b9b7c1ff43ade3f03990035e
kondagoud <kondagoud.merugu@atmecs.com>
2022-03-14 21:21:51 -0700

    Changed the sum method parameters

--- src/main/java/com/sid/twitter/main/AppTest.java
+++ src/main/java/com/sid/twitter/main/AppTest.java
@@ -2,7 +2,7 @@ package com.sid.twitter.main;
 
 public class AppTest {
     public static void main(String[] args) {
-        sum(11, 40);
+        sum(15, 403);
     }
     public static void sum(int a, int b){
         System.out.println(a+b);
